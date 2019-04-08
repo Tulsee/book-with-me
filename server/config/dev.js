075ffe5e5efbdbf03a656a0dec9f77d544ce9bb7
tulsee <ramtulsi022@gmail.com>
2019-04-08 21:22:05 +0545

    add register login and authmiddleware on server

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoDBURI: "mongodb://tulsee1:tulsee1@ds021346.mlab.com:21346/bookwithme"
+  mongoDBURI: "mongodb://tulsee1:tulsee1@ds021346.mlab.com:21346/bookwithme",
+  SECRECT: "Tulsee"
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const Rental = require("./models/rental");
 const Fakedb = require("./fake-db");
 const app = express();
 
 //import routes
 const rentalRoutes = require("./routes/rentals");
+const userRoutes = require("./routes/users");
 
 //db config
 const db = require("./config/dev");
@@ -22,8 +24,13 @@ mongoose
   })
   .catch(err => console.log(err));
 
+//Middleware for bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //use routes
 app.use("/api/v1/rentals", rentalRoutes);
+app.use("/api/v1/users", userRoutes);
 
 // app.get("/rentals", (req, res) => {
 //   res.json({
